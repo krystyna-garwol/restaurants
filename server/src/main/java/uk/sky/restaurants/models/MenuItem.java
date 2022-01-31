b24adc5a50e7be7e6d551609a53935b5f4798208
krystyna-garwol <krystyna.garwol@gmail.com>
2022-01-31 18:59:51 +0000

    updated model document names

--- server/src/main/java/uk/sky/restaurants/models/MenuItem.java
+++ server/src/main/java/uk/sky/restaurants/models/MenuItem.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("menus")
+@Document
 @Getter
 public class MenuItem {
 
--- server/src/main/java/uk/sky/restaurants/models/Restaurant.java
+++ server/src/main/java/uk/sky/restaurants/models/Restaurant.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("restaurants")
+@Document
 @Getter
 @Setter
 public class Restaurant {
