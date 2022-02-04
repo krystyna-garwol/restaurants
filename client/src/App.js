e19d1ca42266ffd59eb870790488193a77254cc0
krystyna-garwol <krystyna.garwol@gmail.com>
2022-02-04 10:46:14 +0000

    remobed footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import Spinner from "./components/Spinner";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 import Home from "./views/Home";
 import CurrentOrder from "./views/CurrentOrder";
 import AddRestaurant from "./views/AddRestaurant";
@@ -97,7 +96,6 @@ const App = () => {
           )}
         />
       </Switch>
-      <Footer />
     </Router>
   );
 };
